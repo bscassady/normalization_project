86e08364bf19e97e78f935b7e362c6eecc312229
gusgusse <75623980+gusgusse@users.noreply.github.com>
2020-12-07 15:34:02 +0100

    Lecture des images rotationnées

---
+++ im.m
@@ -0,0 +1,31 @@
+A1 = imrotate(niftiread('A1.nii'),90);
+A1mask = imrotate(niftiread('A1mask.nii'),90);
+A2 = imrotate(niftiread('A2.nii'),90);
+A2mask = imrotate(niftiread('A2mask.nii'),90);
+A3 = imrotate(niftiread('A3.nii'),90);
+A3mask = imrotate(niftiread('A3mask.nii'),90);
+
+B1 = imrotate(niftiread('B1.nii'),90);
+B1mask = imrotate(niftiread('B1mask.nii'),90);
+B2 = imrotate(niftiread('B2.nii'),90);
+B2mask = imrotate(niftiread('B2mask.nii'),90);
+B3 = imrotate(niftiread('B3.nii'),90);
+B3mask = imrotate(niftiread('B3mask.nii'),90);
+
+C1 = imrotate(niftiread('C1.nii'),90);
+C1mask = imrotate(niftiread('C1mask.nii'),90);
+C2 = imrotate(niftiread('C2.nii'),90);
+C2mask = imrotate(niftiread('C2mask.nii'),90);
+C3 = imrotate(niftiread('C3.nii'),90);
+C3mask = imrotate(niftiread('C3mask.nii'),90);
+
+D1 = imrotate(niftiread('D1.nii'),90);
+D1mask = imrotate(niftiread('D1mask.nii'),90);
+D2 = imrotate(niftiread('D2.nii'),90);
+D2mask = imrotate(niftiread('D2mask.nii'),90);
+D3 = imrotate(niftiread('D3.nii'),90);
+D3mask = imrotate(niftiread('D3mask.nii'),90);
+
+E1 = imrotate(niftiread('E1.nii'),90);
+
+E2mask = imrotate(niftiread('E2mask.nii'),90);
