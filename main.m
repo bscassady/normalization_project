897968369f0fe7016971eb9de640f01c497545bb
Mendioca <arcirot@yahoo.fr>
2020-12-07 22:59:52 +0100

    tracer du rectangle en tant que bounding bos

--- main.m
+++ main.m
@@ -3,5 +3,5 @@ subplot(1,2,1);
 imagesc(V);
 maskV = binary_mask(V, 80);
 subplot(1,2,2); imagesc(maskV);
-propsV = regionprops(maskV, 'BoundingBox');
-bbxV = vertcat(propsV.BoundingBox);
+st = regionprops(maskV, 'BoundingBox');
+rectangle('Position', [st.BoundingBox(1), st.BoundingBox(2), st.BoundingBox(3), st.BoundingBox(4)],'EdgeColor','r','LineWidth',2 )
