863a1b33b74012b509d353960d760f9c4e8f93fc
bscassady <baptiste.sorin@insa-lyon.fr>
2020-12-15 07:51:36 +0100

    main correction

--- main.m
+++ main.m
@@ -1,3 +1,67 @@
+brain = B1;
+
+
+V = niftiread('data/B1.nii');
+figure(1);
+subplot(1,2,1);
+imagesc(V);
+%Create a binary mask of the image
+maskV = binary_mask(V, 80);
+subplot(1,2,2);imshow(maskV);
+%CENTERING AND DRAWING ELLIPSE
+%bonding ellipse of the original image
+el = regionprops(maskV, {'Centroid', 'MajorAxisLength', 'MinorAxisLength', 'Orientation'});
+maskV = imrotate(maskV,90);
+[M,N] = size(maskV);
+
+propsV = regionprops(maskV, 'BoundingBox');
+bbxV = vertcat(propsV.BoundingBox);
+center = el(1).Centroid(2); 
+shift1 = center - N/2;%Shift necessary to center the image
+V_shift = shift_image(maskV, shift1); 
+figure(2);
+subplot(1,2,1);imshow(V_shift);% shift the center of the ellipse on the median vertical line of the image
+angle1 = -el(1).Orientation;
+V_rot = imrotate(V_shift, angle1);% rotate the brain so that major axis of the ellipse alings with the median line
+[M2, N2] = size(V_rot);
+if mod(N2,2)==1 
+    N2 = N2-1;
+    V_rot = V_rot(:, 1:N2);
+end
+V_line = V_rot;
+V_line(:,int16(N2/2))=ones(M2,1)*255; %The brain should be centered 
+
+
+
+
+subplot(1,2,2); imshow(V_line);
+
+
+
+% Create Bounding ellipse and display it on mask
+el = regionprops(V_line, {'Centroid', 'MajorAxisLength', 'MinorAxisLength', 'Orientation'});
+
+subplot(1,2,2); imshow(V_line);
+
+t = linspace(0,2*pi,50);
+hold on
+    for k = 1:length(el)
+        a = el(k).MajorAxisLength/2;
+        b = el(k).MinorAxisLength/2;
+        Xc = el(k).Centroid(1);
+        Yc = el(k).Centroid(2);
+        phi = deg2rad(-el(k).Orientation);
+        x = Xc + a*cos(t)*cos(phi) - b*sin(t)*sin(phi);
+        y = Yc + a*cos(t)*sin(phi) + b*sin(t)*cos(phi);
+        plot(x,y,'r','Linewidth',5);
+        plot(Xc,Yc,'b','Linewidth',5);
+    end
+
+hold off
+
+
+   
+
 %%
     
 
@@ -28,17 +92,12 @@ x = [(BB.BoundingBox(1) + BB.BoundingBox(3)/2) (BB.BoundingBox(1) + BB.BoundingB
 y = [BB.BoundingBox(2) (BB.BoundingBox(2) + BB.BoundingBox(4))];
 subplot(1,2,1), line(x,y, 'Color','red','LineWidth',2); %Approximation not good
 
-% 2.3.1 Superposition des deux hémisphères
-proportion_pixels_communs = similarity(B1, B1, 80);
-proportion_pixels_communs
-
 % Create a method to find line to line the position of the longitudinal fissure and give a map of uncertainty of its position
 % % Loop to go through image inside bounding box
 % % Get intensity profile and detect when == 0 : if == 0 draw linear
 % regression on original image
 % % and calculate distance from this point to line D
 % % Gather distances in a map and connect dots
-
 dist_map = containers.Map;
 for x = BB.BoundingBox(1) : (BB.BoundingBox(1) + BB.BoundingBox(3))
     for y = BB.BoundingBox(2) : (BB.BoundingBox(2) + BB.BoundingBox(4))
